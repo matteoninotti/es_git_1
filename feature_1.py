cc1e9a644b14adf311b7e24da1089157b8dbcb64
matteoninotti <mattyninotti@gmail.com>
2026-05-20 12:57:21 +0200

    feature 1 vero

---
+++ feature_1.py
@@ -0,0 +1,23 @@
+import os
+from datetime import datetime
+import time
+
+def create_folders() -> str:
+  """crea cartelle di lavoro se non esistenti"""
+  f_list = os.listdir()
+  if "in" not in f_list:
+    os.mkdir("./in")
+    ret = "cartella \"in\" inizializzate correttamente"
+  else: ret = "cartella \"in\" già esistente"
+  return ret
+
+def create_file():
+  file_name = datetime.now().strftime("%Y%m%d_%H%M%S")
+  f = open(f"in/{file_name}", "x")
+  return f"creato file \"./in/{file_name}\""
+
+print(create_folders())
+
+while True:
+  print(create_file())
+  time.sleep(10)
