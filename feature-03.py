63c2be5016e1cced07654a619682fdadd965f9c8
EDU-ITS\umberto.farabini <umberto.farabini@edu-its.it>
2026-05-20 12:35:45 +0200

    creato file 3 che controlla se ci sono file nella cartella ./processing e li sposta nella cartella ./out

---
+++ feature-03.py
@@ -0,0 +1,32 @@
+import time
+import os
+import shutil
+
+
+cdw = os.getcwd()
+dir_in = "./processing"
+dir_out = "./out"
+
+
+os.makedirs(dir_out, exist_ok=True)
+os.makedirs(dir_in, exist_ok=True)  
+
+while True:
+
+        items = os.listdir(dir_in)
+        
+
+        files = [i for i in items if os.path.isfile(os.path.join(dir_in, i))]
+        
+        if files:
+            print(f"{time.strftime('%X')}: Trovati {len(files)} file, spostamento in corso...")
+            for file_name in files:
+                src_path = os.path.join(dir_in, file_name)
+                dst_path = os.path.join(dir_out, file_name)
+                
+
+                shutil.move(src_path, dst_path)
+            print("Spostamento completato.")
+            
+
+        time.sleep(30)
